440e6a0f0ee2bac77176adc93b5f40c8b5f90809
seanoshea <oshea.ie@gmail.com>
2013-12-21 21:34:45 +0000

    Some tests.

--- BluePlaquesLondonTests/Utils/BPLPlacemarkFeatureDescriptionTests.m
+++ BluePlaquesLondonTests/Utils/BPLPlacemarkFeatureDescriptionTests.m
@@ -23,6 +23,17 @@
 @property (nonatomic, copy) NSString *wanamaker;
 @property (nonatomic, copy) NSString *gainsborough;
 @property (nonatomic, copy) NSString *holst;
+@property (nonatomic, copy) NSString *nicholson;
+@property (nonatomic, copy) NSString *mcmillan;
+@property (nonatomic, copy) NSString *tait;
+@property (nonatomic, copy) NSString *hutchinson;
+@property (nonatomic, copy) NSString *mann;
+@property (nonatomic, copy) NSString *pevsner;
+@property (nonatomic, copy) NSString *taylor;
+@property (nonatomic, copy) NSString *walton;
+@property (nonatomic, copy) NSString *popper;
+@property (nonatomic, copy) NSString *hazlitt;
+@property (nonatomic, copy) NSString *adam;
 
 @end
 
@@ -34,12 +45,44 @@
     self.wanamaker = @"WANAMAKER, Sam (1919-1993)<br> The man behind Shakespeare's Globe<br> New Globe Buildings, Bankside, SE1<br> Southwark 2003<br> Southwark Council Plaque";
     self.gainsborough = @"GAINSBOROUGH, Thomas (1727-1788)<br> Artist, lived here<br> 80 - 82 Pall Mall, SW1<br> Westminster 1951<br> <em>Note: Replaces plaque up in 1881 by RSA at No. 80.</em>";
     self.holst = @"HOLST, Gustav (1874-1934)<br>Composer, wrote, <em>The Planets</em>, and, taught here<br>St Paul's Girls' School, Brook Green, W8<br>Hammersmith and Fulham 2004";
+    self.nicholson = @"NICHOLSON, William<br>1872-1949<br>Painter and Printmaker lived here 1904-1906<br>1 Pilgrim's Lane, Hampstead, London<br>NW3 1SJ<br>Camden 2010";
+    self.mcmillan = @"McMILLAN, Margaret<br>1860-1931<br>RACHEL McMILLAN 1859-1917 MARGARET McMILLAN 1860-1931 Pioneers of Nursery Education lodged here<br>51 Tweedy Road, Bromley, London<br>BR1 3NH<br>Bromley 2009";
+    self.tait = @"TAIT, Thomas Smith<br>1882-1954<br>Architect lived here<br>Gates House, Wyldes Close, Hampstead, London<br>NW11 7JB<br>Barnet 2006";
+    self.hutchinson = @"HUTCHINSON, Leslie 'Hutch'<br>1900-1969<br>Singer and Pianist lived here 1929-1967<br>31 Steele's Road, Chalk Farm<br>NW3 4RE<br>Camden 2012";
+    self.mann = @"MANN, Dame Ida<br>1893-1983<br>Ophthalmologist lived here 1902-1934<br>13 Minster Road, West Hampstead<br>NW2 3SE<br>Camden 2012";
+    self.pevsner = @"PEVSNER, Sir Nikolaus<br>1902-1983<br>Architectural Historian lived here from 1936 until his death<br>2 Wildwood Terrace, Hampstead, London<br>NW3 7HT<br>Camden 2007";
+    self.taylor = @"TAYLOR, A.J.P.<br>1906-1990<br>Historian and Broadcaster lived here<br>13 St Mark's Crescent, Primrose Hill<br>NW1 7TS<br>Camden 2012";
+    self.walton = @"WALTON, Sir William<br>1902-1983<br>Composer lived here<br>Lowndes Cottage, 8 Lowndes Place, Belgravia, London<br>SW1X 8DD<br>Westminster 2009";
+    self.popper = @"POPPER, Karl<br>16 Burlington Rise, EN4<br>London Borough of Barnet";
+    self.hazlitt = @"HAZLITT, William (1778-1830) Essayist, died here.<br> 6 Frith Street, W1<br> Westminster 1905";
+    self.adam = @"ADAM, Robert (1728-1792), Architect; Thomas HOOD (1799-1845), Poet; John GALSWORTHY (1867-1933), Novelist and Playwright; Sir James BARRIE (1860-1937), Dramatist; and other eminent artists and writers lived here.<br>1-3 Robert Street, Adelphi, WC2<br>Westminster 1950";
 }
 
 - (void)testExtractName
 {
-    NSString *name = self.wanamaker.name;
-    XCTAssert([name isEqualToString:@"WANAMAKER, Sam"], @"The name should only contain the person's full name and not the dates");
+    NSString *wanamaker = self.wanamaker.name;
+    NSString *gainsborough = self.gainsborough.name;
+    NSString *holst = self.holst.name;
+    NSString *nicholson = self.nicholson.name;
+    NSString *mcmillan = self.mcmillan.name;
+    NSString *tait = self.tait.name;
+    NSString *hutchinson = self.hutchinson.name;
+    NSString *mann = self.mann.name;
+    NSString *pevsner = self.pevsner.name;
+    NSString *taylor = self.taylor.name;
+    NSString *walton = self.walton.name;
+    
+    XCTAssert([wanamaker isEqualToString:@"WANAMAKER, Sam"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([gainsborough isEqualToString:@"GAINSBOROUGH, Thomas"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([holst isEqualToString:@"HOLST, Gustav"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([nicholson isEqualToString:@"NICHOLSON, William"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([mcmillan isEqualToString:@"McMILLAN, Margaret"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([tait isEqualToString:@"TAIT, Thomas Smith"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([hutchinson isEqualToString:@"HUTCHINSON, Leslie 'Hutch'"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([mann isEqualToString:@"MANN, Dame Ida"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([pevsner isEqualToString:@"PEVSNER, Sir Nikolaus"], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([taylor isEqualToString:@"TAYLOR, A.J.P."], @"The name should only contain the person's full name and not the dates");
+    XCTAssert([walton isEqualToString:@"WALTON, Sir William"], @"The name should only contain the person's full name and not the dates");
 }
 
 - (void)testExtractTitle
@@ -56,14 +99,40 @@
 
 - (void)testExtractOccupation
 {
-    NSString *occupation = self.wanamaker.occupation;
-    XCTAssert([occupation isEqualToString:@"The man behind Shakespeare's Globe"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    NSString *wanamaker = self.wanamaker.occupation;
+    NSString *gainsborough = self.gainsborough.occupation;
+    NSString *nicholson = self.nicholson.occupation;
+    NSString *mcmillan = self.mcmillan.occupation;
+    NSString *tait = self.tait.occupation;
+    NSString *hutchinson = self.hutchinson.occupation;
+    NSString *mann = self.mann.occupation;
+    NSString *pevsner = self.pevsner.occupation;
+    NSString *taylor = self.taylor.occupation;
+    NSString *walton = self.walton.occupation;
+
+    XCTAssert([wanamaker isEqualToString:@"The man behind Shakespeare's Globe"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([gainsborough isEqualToString:@"Artist, lived here"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([nicholson isEqualToString:@"Painter and Printmaker lived here 1904-1906"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([mcmillan isEqualToString:@"RACHEL McMILLAN 1859-1917 MARGARET McMILLAN 1860-1931 Pioneers of Nursery Education lodged here"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([tait isEqualToString:@"Architect lived here"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([hutchinson isEqualToString:@"Singer and Pianist lived here 1929-1967"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([mann isEqualToString:@"Ophthalmologist lived here 1902-1934"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([pevsner isEqualToString:@"Architectural Historian lived here from 1936 until his death"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([taylor isEqualToString:@"Historian and Broadcaster lived here"], @"The occupation should only include the reason the plaque was commerated in the first place");
+    XCTAssert([walton isEqualToString:@"Composer lived here"], @"The occupation should only include the reason the plaque was commerated in the first place");
 }
 
 - (void)testExtractAddress
 {
-    NSString *address = self.wanamaker.address;
-    XCTAssert([address isEqualToString:@"New Globe Buildings, Bankside, SE1"], @"The address should only include the plaque's human-readable location");
+    NSString *wanamaker = self.wanamaker.address;
+    NSString *popper = self.popper.address;
+    NSString *hazlitt = self.hazlitt.address;
+    NSString *adam = self.adam.address;
+
+    XCTAssert([wanamaker isEqualToString:@"New Globe Buildings, Bankside, SE1"], @"The address should only include the plaque's human-readable location");
+    XCTAssert([popper isEqualToString:@"16 Burlington Rise, EN4"], @"The address should only include the plaque's human-readable location");
+    XCTAssert([hazlitt isEqualToString:@"6 Frith Street, W1"], @"The address should only include the plaque's human-readable location");
+    XCTAssert([adam isEqualToString:@"1-3 Robert Street, Adelphi, WC2"], @"The address should only include the plaque's human-readable location");
 }
 
 - (void)testExtractNote
