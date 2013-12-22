133cf39e6d1896adec053f43b9bdbe3feaaaf20a
seanoshea <oshea.ie@gmail.com>
2013-12-22 18:03:54 +0000

    Multiple people at one location.

---
+++ BluePlaquesLondon/Classes/Controller/BPLDetailChooserViewController.m
@@ -0,0 +1,56 @@
+/*
+ Copyright 2012 - 2013 UpwardsNorthwards
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "BPLDetailChooserViewController.h"
+
+#import "SimpleKMLPlacemark.h"
+#import "UIColor+BluePlaquesLondon.h"
+
+@implementation BPLDetailChooserViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.navigationController.title = NSLocalizedString(@"Choose one", nil);
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.markers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"BluePlaquesLondonMultipleCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    SimpleKMLPlacemark *pm = self.markers[indexPath.row];
+    cell.textLabel.textColor = [UIColor darkBlueColour];
+    cell.textLabel.text = pm.name;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
--- BluePlaquesLondon/Classes/Utils/BPLConstants.m
+++ BluePlaquesLondon/Classes/Utils/BPLConstants.m
@@ -21,6 +21,7 @@
 NSString * const BPLKMZFilename = @"blueplaques";
 NSString * const BPLMapDetailViewControllerSegue = @"BPLMapDetailViewControllerSegue";
 NSString * const BPLWikipediaViewControllerSegue = @"BPLWikipediaViewControllerSegue";
+NSString * const BPLDetailChooserViewControllerSegue = @"BPLDetailChooserViewControllerSegue";
 
 #pragma mark - 3rd Party Keys
 
