078187b0deea28f5be7bba39b102ce61edab7c16
seanoshea <oshea.ie@gmail.com>
2015-01-03 21:31:44 +0000

    LICENSE

--- BluePlaquesLondon/Classes/View/BPLLabel.m
+++ BluePlaquesLondon/Classes/View/BPLLabel.m
@@ -1,5 +1,5 @@
 /*
- Copyright 2013 - 2014 Sean O' Shea
+ Copyright 2013 - 2015 Sean O'Shea
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
