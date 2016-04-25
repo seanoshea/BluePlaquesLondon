b2dce9fe66f67324646ab51433d7d795d1dbfb7e
seanoshea <oshea.ie@gmail.com>
2016-04-25 08:03:26 -0400

    Per issue #108 - Does the model return before I can actually set the expectation?

--- BluePlaquesLondonTests/Model/BPLWikipediaViewModelTest.m
+++ BluePlaquesLondonTests/Model/BPLWikipediaViewModelTest.m
@@ -62,18 +62,18 @@
     
     XCTestExpectation *expectation = [self expectationWithDescription:@"Testing [WikipediaViewModel retrieveWikipediaUrlWithCompletionBlock]"];
     
+    [self waitForExpectationsWithTimeout:10.0 handler:^(NSError *error) {
+        if (error) {
+            XCTFail(@"Expectation Failed with error: %@", error);
+        }
+    }];
+    
     [self.model retrieveWikipediaUrlWithCompletionBlock:^(NSURLRequest *urlRequest, NSError *error) {
         if (error == nil) {
             XCTAssert([urlRequest.URL.absoluteString isEqualToString:@"https://en.wikipedia.org/wiki/Winston_Churchill"], @"The absolute URLs should be equal");
             [expectation fulfill];
         } else {
             XCTFail(@"There was an error while retrieving the wikipedia URL: %@", error);
-        }
-    }];
-
-    [self waitForExpectationsWithTimeout:10.0 handler:^(NSError *error) {
-        if (error) {
-            XCTFail(@"Expectation Failed with error: %@", error);
         }
     }];
 }
